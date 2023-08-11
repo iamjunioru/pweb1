545301cd7f5d3c383988eac8e7642dc03c3af59e
Junior <iamjunioru@gmail.com>
2023-08-11 01:23:23 -0300

    feat: route / and /all

--- pratica2N2/app.py
+++ pratica2N2/app.py
@@ -1,8 +1,7 @@
-# app.py
 from flask import Flask
 from flask_restful import Api
 from models import db, ma
-from resources import TutorResource, PetResource
+from resources import TutorResource, PetResource, GetAllResource
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -13,8 +12,14 @@ ma.init_app(app)
 with app.app_context():
     db.create_all()
 
+api.add_resource(GetAllResource, '/all')
 api.add_resource(TutorResource, '/tutor', '/tutor/<int:tutor_id>')
 api.add_resource(PetResource, '/pet', '/pet/<int:pet_id>')
+
+
+@app.route('/')
+def welcome():
+    return 'Bem-vindo ao meu aplicativo Flask com Flask-RESTful e SQLAlchemy!'
 
 if __name__ == '__main__':
     app.run(debug=True)
