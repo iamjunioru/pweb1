d2f12cfe450d51550f0f02c77e2ec30a20370953
Junior <iamjunioru@gmail.com>
2023-08-11 01:52:19 -0300

    chore: welcome :)

--- pratica2N2/app.py
+++ pratica2N2/app.py
@@ -4,7 +4,7 @@ from models import db, ma
 from resources import TutorResource, PetResource, GetAllResource
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 api = Api(app)
 db.init_app(app)
 ma.init_app(app)
@@ -12,14 +12,16 @@ ma.init_app(app)
 with app.app_context():
     db.create_all()
 
-api.add_resource(GetAllResource, '/all')
-api.add_resource(TutorResource, '/tutor', '/tutor/<int:tutor_id>')
-api.add_resource(PetResource, '/pet', '/pet/<int:pet_id>')
 
-
-@app.route('/')
+@app.route("/")
 def welcome():
-    return 'Bem-vindo ao meu aplicativo Flask com Flask-RESTful e SQLAlchemy!'
+    return "Welcome to the Petshop API! Use /tutor/:id and /pet/:id to check."
 
-if __name__ == '__main__':
+
+api.add_resource(GetAllResource, "/all")
+api.add_resource(TutorResource, "/tutor", "/tutor/<int:tutor_id>")
+api.add_resource(PetResource, "/pet", "/pet/<int:pet_id>")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
