341ae682b8e09159aaccf13e89025ca0055ecc38
Junior <iamjunioru@gmail.com>
2023-08-11 01:52:47 -0300

    style: jsonify implement

--- pratica2N2/resources.py
+++ pratica2N2/resources.py
@@ -2,6 +2,7 @@ from flask_restful import Resource, reqparse
 from flask import jsonify
 from models import db, Tutor, Pet, TutorSchema, PetSchema
 
+
 class GetAllResource(Resource):
     def get(self):
         tutors = Tutor.query.all()
@@ -9,37 +10,38 @@ class GetAllResource(Resource):
 
         for tutor in tutors:
             tutor_data = {
-                'id': tutor.id,
-                'nome': tutor.nome,
-                'pets': [{'id': pet.id, 'nome': pet.nome} for pet in tutor.pets]
+                "id": tutor.id,
+                "nome": tutor.nome,
+                "pets": [{"id": pet.id, "nome": pet.nome} for pet in tutor.pets],
             }
             tutors_data.append(tutor_data)
 
         return tutors_data, 200
-    
+
+
 class TutorResource(Resource):
     def get(self, tutor_id=None):
         if tutor_id is None:
             tutors = Tutor.query.all()
-            return TutorSchema(many=True).dump(tutors), 200
+            tutors_data = []
 
         tutor = Tutor.query.get(tutor_id)
         if not tutor:
-            return {'message': 'Tutor not found'}, 404
+            return {"message": "Tutor not found"}, 404
 
         tutor_data = {
-            'id': tutor.id,
-            'nome': tutor.nome,
-            'pets': [{'id': pet.id, 'nome': pet.nome} for pet in tutor.pets]
+            "id": tutor.id,
+            "nome": tutor.nome,
+            "pets": [{"id": pet.id, "nome": pet.nome} for pet in tutor.pets],
         }
 
-        return tutor_data, 200
+        return jsonify(tutor_data), 200
 
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('nome_tutor', type=str, required=True)
+        parser.add_argument("nome_tutor", type=str, required=True)
         args = parser.parse_args()
-        tutor = Tutor(nome=args['nome_tutor'])
+        tutor = Tutor(nome=args["nome_tutor"])
         db.session.add(tutor)
         db.session.commit()
         tutor_schema = TutorSchema()
@@ -49,28 +51,28 @@ class TutorResource(Resource):
     def put(self, tutor_id):
         tutor = Tutor.query.get(tutor_id)
         if not tutor:
-            return {'message': 'Tutor not found'}, 404
-        
+            return {"message": "Tutor not found"}, 404
+
         parser = reqparse.RequestParser()
-        parser.add_argument('nome_tutor', type=str, required=True)
+        parser.add_argument("nome_tutor", type=str, required=True)
         args = parser.parse_args()
-        
-        tutor.nome = args['nome_tutor']
+
+        tutor.nome = args["nome_tutor"]
         db.session.commit()
 
-        return {'message': 'Tutor updated successfully'}, 200
+        return {"message": "Tutor updated successfully"}, 200
 
     def delete(self, tutor_id):
         tutor = Tutor.query.get(tutor_id)
         if not tutor:
-            return {'message': 'Tutor not found'}, 404
+            return {"message": "Tutor not found"}, 404
 
         if tutor.pets:
-            return {'message': 'Cannot delete tutor with associated pets'}, 400
-        
+            return {"message": "Cannot delete tutor with associated pets"}, 400
+
         db.session.delete(tutor)
         db.session.commit()
-        return {'message': 'Tutor deleted successfully'}, 204
+        return {"message": "Tutor deleted successfully"}, 204
 
 
 class PetResource(Resource):
@@ -78,7 +80,7 @@ class PetResource(Resource):
         if pet_id is not None:
             pet = Pet.query.get(pet_id)
             if not pet:
-                return {'message': 'Pet not found'}, 404
+                return {"message": "Pet not found"}, 404
             pet_schema = PetSchema()
             pet_data = pet_schema.dump(pet)
             return pet_data, 200
@@ -91,15 +93,15 @@ class PetResource(Resource):
 
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('nome_pet', type=str, required=True)
-        parser.add_argument('tutor_id', type=int, required=True)
+        parser.add_argument("nome_pet", type=str, required=True)
+        parser.add_argument("tutor_id", type=int, required=True)
         args = parser.parse_args()
 
-        tutor = Tutor.query.get(args['tutor_id'])
+        tutor = Tutor.query.get(args["tutor_id"])
         if not tutor:
-            return {'message': 'Tutor not found'}, 404
-        
-        pet = Pet(nome=args['nome_pet'], tutor=tutor)
+            return {"message": "Tutor not found"}, 404
+
+        pet = Pet(nome=args["nome_pet"], tutor=tutor)
         db.session.add(pet)
         db.session.commit()
         pet_schema = PetSchema()
@@ -109,13 +111,13 @@ class PetResource(Resource):
     def put(self, pet_id):
         pet = Pet.query.get(pet_id)
         if not pet:
-            return {'message': 'Pet not found'}, 404
-        
+            return {"message": "Pet not found"}, 404
+
         parser = reqparse.RequestParser()
-        parser.add_argument('nome_pet', type=str, required=True)
+        parser.add_argument("nome_pet", type=str, required=True)
         args = parser.parse_args()
-        
-        pet.nome = args['nome_pet']
+
+        pet.nome = args["nome_pet"]
         db.session.commit()
         pet_schema = PetSchema()
         pet_data = pet_schema.dump(pet)
@@ -124,8 +126,8 @@ class PetResource(Resource):
     def delete(self, pet_id):
         pet = Pet.query.get(pet_id)
         if not pet:
-            return {'message': 'Pet not found'}, 404
-        
+            return {"message": "Pet not found"}, 404
+
         db.session.delete(pet)
         db.session.commit()
-        return {'message': 'Pet deleted'}, 204
+        return {"message": "Pet deleted"}, 204
